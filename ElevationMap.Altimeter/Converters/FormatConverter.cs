b2ba2d8a2c86959bc834882b9a79f2f11cdd4ae4
nvsnkv <nvsnkv@live.com>
2014-11-13 21:08:29 +0300

    TrackerView

---
+++ ElevationMap.Altimeter/Converters/FormatConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace NV.ElevationMap.Altimeter.Converters
+{
+    public class FormatConverter:IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+
+            return parameter != null 
+                        ? string.Format(parameter.ToString(), value)
+                        : value;
+            
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
