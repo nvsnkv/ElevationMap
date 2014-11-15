51d45e11235bc9d2bd14d80ee19ed759d4fefe3e
nvsnkv <nvsnkv@live.com>
2014-11-15 21:16:51 +0300

    empty settings view

---
+++ ElevationMap.Altimeter/Views/Setings/SettingsView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls;
+
+namespace NV.ElevationMap.Altimeter.Views.Setings
+{
+    public partial class SettingsView : Grid
+    {
+        public SettingsView()
+        {
+            InitializeComponent();
+        }
+
+         
+    }
+}
